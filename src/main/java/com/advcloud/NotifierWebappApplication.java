9f78dcbcc4906d834eb93eb49b14ca3cbc2e40fa
SridharPrasad-Panneerselvamçclear <panneerselvam.s@northeastern.edu>
2021-03-11 16:39:23 -0500

    notifier webapp base commit

---
+++ src/main/java/com/advcloud/NotifierWebappApplication.java
@@ -0,0 +1,13 @@
+package com.advcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifierWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifierWebappApplication.class, args);
+	}
+
+}
